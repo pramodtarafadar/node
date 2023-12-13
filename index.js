99f8882b25bbe8884e06319c0d9e4793376c00df
Pramod Tarafadar <pdtarafadar@gmail.com>
2023-12-13 10:45:35 +0530

    added db and first API

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-const express = require('express')
-const app = express()
+//SQLite Database Initialization//
+
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+app.use(express.json())
+
+const dbPath = path.join(__dirname, "demo.db");
+
+let db = null;
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
-})
 
-app.get("/", (req, res) => {
-    res.send("Hello Pramod")
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(PORT, () => {
+      console.log(`Server Running at http://localhost:${PORT}/`);
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+app.get("/", async(req, res) => {
+    const query = `
+    SELECT * FROM users;`
+    const result = await db.all(query)
+    res.send(result)
 })
